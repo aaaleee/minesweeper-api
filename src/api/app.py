fea0a140ac46363821fd6799b89579d5ceab5738
aaaleee <hello@alericatti.com>
2020-06-12 23:16:21 -0300

    Add jwt decorator and register endpoint

--- src/api/app.py
+++ src/api/app.py
@@ -22,5 +22,39 @@ db = SQLAlchemy(app)
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(255))
+
+
+def jwt_required(f):
+   @wraps(f)
+   def decorator(*args, **kwargs):
+      token = None
+      if 'x-access-tokens' in request.headers:
+         token = request.headers['x-access-tokens']
+
+      if not token:
+         return jsonify({'message': 'Missing token'})
+
+      try:
+         data = jwt.decode(token, app.config[SECRET_KEY])
+         current_user = Users.query.filter_by(email=data['email']).first()
+      except:
+         return jsonify({'message': 'Invalid token'})
+
+      return f(current_user, *args, **kwargs)
+   return decorator
+
+
+@app.route('/register', methods=['POST'])
+def register():
+   data = request.get_json()
+
+   hashed_password = generate_password_hash(data['password'], method='sha256')
+
+   user = Users(email=data['email'], password=hashed_password)
+   db.session.add(user)
+   db.session.commit()
+   user_data = {"id": user.id, "email": user.email}
+
+   return jsonify({'message': 'Registered successfully', "user": user_data})
